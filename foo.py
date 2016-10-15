e05fad8b48d627b3fd927d05177607bf5c1cb3c1
Aaron Schif <aschif@netdevgroup.com>
2016-10-15 15:12:48 -0500

    Glade

--- foo.py
+++ foo.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+
+import signal
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
@@ -11,12 +14,13 @@ Gst.init(None)
 Gst.init_check(None)
 
 
-class VideoWidget(Gtk.DrawingArea):
-    def __init__(self, pipeline):
-        super().__init__()
-
+class PipelineManager(object):
+    def __init__(self, window, pipeline):
+        self.window = window
         if isinstance(pipeline, str):
             pipeline = Gst.parse_launch(pipeline)
+
+        self.pipeline = pipeline
 
         bus = pipeline.get_bus()
         bus.set_sync_handler(self.bus_callback)
@@ -27,7 +31,7 @@ class VideoWidget(Gtk.DrawingArea):
             if GstVideo.is_video_overlay_prepare_window_handle_message(message):
                 Gdk.threads_enter()
                 Gdk.Display.get_default().sync()
-                win = drawing.get_property('window')
+                win = self.window.get_property('window')
 
                 if isinstance(win, GdkX11.X11Window):
                     message.src.set_window_handle(win.get_xid())
@@ -38,25 +42,21 @@ class VideoWidget(Gtk.DrawingArea):
         return Gst.BusSyncReply.PASS
 
 
+
+
 # pipeline = Gst.parse_launch('videotestsrc ! xvimagesink sync=false')
-pipeline = Gst.parse_launch('udpsrc port=1234 ! application/x-rtp, payload=12 ! rtph264depay ! avdec_h264 ! xvimagesink sync=false')
+pipeline = 'udpsrc port=1234 ! application/x-rtp, payload=12 ! rtph264depay ! avdec_h264 ! xvimagesink sync=false'
 # gst-launch-1.0 v4l2src device=/dev/video0 ! 'video/x-raw,width=640,height=480' !  x264enc pass=qual quantizer=2 tune=zerolatency ! rtph264pay ! udpsink host=127.0.0.1 port=1234
 
-drawing = VideoWidget(pipeline)
-drawing.set_size_request(640, 480)
 
-window = Gtk.Window()
-window.set_size_request(300, 50)
-main_box = Gtk.HBox()
-entry = Gtk.Entry()
-main_box.pack_start(entry, True, True, 0)
-log = Gtk.TextView()
-log.set_editable(False)
-log.set_size_request(30, 30)
-main_box.pack_start(log, True, True, 0)
-main_box.pack_start(drawing, False, False, 0)
-main_box.pack_start(Gtk.Button('Foo'), True, True, 0)
-window.add(main_box)
+builder = Gtk.Builder()
+builder.add_from_file('foo.glade')
+
+video_area = builder.get_object('video_area')
+
+pipeline = PipelineManager(video_area, pipeline)
+
+window = builder.get_object("main_window")
 window.show_all()
 window.connect('destroy', lambda w: Gtk.main_quit())
 
