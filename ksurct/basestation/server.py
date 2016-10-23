b1bc42affcb5708540e41f6e29840f0344c049b4
Aaron Schif <aschif@netdevgroup.com>
2016-10-23 11:51:26 -0500

    Add full protocol

--- ksurct/basestation/server.py
+++ ksurct/basestation/server.py
@@ -11,21 +11,28 @@ from .messaging import NewSensorData
 Controller.init()
 
 
-class XboxComponent(object):
-    def __init__(self, **kwargs):
-        self.parts = kwargs
-        self.state = {k: None for k in kwargs}
+class Toggle(object):
+    def __init__(self, cb):
+        self.state = False
+        self.callback = cb
 
-    def check_updates(self):
-        needs_update = False
-        for k, v in self.parts.items():
-            new_value = v()
-            old_value = self.state[k]
-            if new_value != old_value:
-                needs_update = True
+    def __call__(self):
+        self.state ^= self.cb()
+        return self.state
 
-            self.state[k] = new_value
-        return needs_update
+
+def calculate_motor_speed(self, x, y, mod):
+        r, l = -y, -y
+        r += -x/4
+        l += x/4
+
+        modifier = 120
+        if mod:
+            modifier = 60
+
+        r = int(r*modifier)
+        l = int(l*modifier)
+        return r, l
 
 
 class Server(Thread):
@@ -44,7 +51,12 @@ class Server(Thread):
         loop.close()
 
     async def main_loop(self):
-        lights = XboxComponent(on=self.xbox.get_y)
+        lights = Toggle(self.xbox.get_y)
+        breaks = self.xbox.get_b
+        motors = lambda: calculate_motor_speed(x=self.xbox.get_left_x(), y=self.xbox.get_left_y(), mod=self.xbox.get_x())
+        trigger_arm = lambda: self.xbox.get_right_trigger() > 0.9
+        close_claw = self.xbox.get_a
+        degree_camera = lambda: controller.get_right_x() * 190
 
         async with websockets.connect('ws://10.243.81.158:9002/') as websocket:
             while True:
@@ -52,9 +64,25 @@ class Server(Thread):
 
                 robot_msg = Robot()
 
-                if lights.check_updates():
-                    robot_msg.headlights.update = True
-                    robot_msg.headlights.on = lights.state['on']
+                robot_msg.headlights.update = True
+                robot_msg.headlights.on = lights()
+
+                right_speed, left_speed = motors()
+                robot_msg.motor_right_rpm.update = True
+                robot_msg.motor_right_rpm.speed = right_speed
+                robot_msg.motor_right_rpm.breaks = breaks()
+                robot_msg.motor_left_rpm.update = True
+                robot_msg.motor_left_rpm.speed = left_speed
+                robot_msg.motor_left_rpm.breaks = breaks()
+
+                robot_msg.claw.update = True
+                robot_msg.claw.degree = close_claw() * 90
+
+                robot_msg.arm.update = True
+                robot_msg.arm.degree = 5304 if trigger_arm() else 3120
+
+                robot_msg.camera.update = True
+                robot_msg.camera.degree = 190 - int(camera_degree())
 
                 ser_msg = robot_msg.SerializeToString()
 
